9ab7ae1c9883b20cec84b7f61bc4bb5fb40a2e37
selga <osmargastonescobar@gmail.com>
2024-06-09 01:57:29 -0300

    agregar texto al h1

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const titulo=document.querySelector("h1");
 const CAMBIAR_COLOR=document.querySelector("#cambiarcolor");
+const nombreInput=document.querySelector("#nombre");
+const apellidoInput=document.querySelector("#apellido");
 
 const cambiarColor=function(){
     titulo.classList.add("color");
 }
 CAMBIAR_COLOR.addEventListener("click",cambiarColor);
+
+function actualizarTitulo() {
+    const nombre = nombreInput.value;
+    const apellido = apellidoInput.value;
+    titulo.textContent = `Bienvenido ${nombre} ${apellido}`;
+}
+
+    nombreInput.addEventListener('keyup', actualizarTitulo); 
+    apellidoInput.addEventListener('keyup', actualizarTitulo);
+    ''
